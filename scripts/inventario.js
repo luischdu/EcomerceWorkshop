d82360434633dcdc326e57dd39752846547dfc94
jonathz <72336663+jonathz@users.noreply.github.com>
2021-04-30 01:51:32 -0500

    Cambiando de precios correctamente por tallas y modelo

--- scripts/inventario.js
+++ scripts/inventario.js
@@ -1,68 +1,73 @@
-let bodega = 
-     [{     nombre:"Crown Sweatshirt",
-            imagenNo1:'https://imagizer.imageshack.com/img922/6082/PRGgVV.png',
-            imagenNo2:'https://imagizer.imageshack.com/img924/2289/7aHA06.png',
-            imagenNo3:'https://imagizer.imageshack.com/img923/5338/yDweCx.png',
-            imagenNo4:'https://imagizer.imageshack.com/img924/1899/CAZZuK.png',
-            tallaXS: 55000 ,
-            tallaS: 75000 ,
-            tallaM: 80000 ,
-            tallaL: 85000 ,
-            tallaXL: 90000 ,
-            tallaXXL: 100000 ,
-            inventario: 7,
-            descripcion: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Nibh venenatis cras sed felis eget. At in tellus integer feugiat scelerisque varius morbi. Morbi tempus iaculis urna id volutpat. Eget nunc scelerisque viverra mauris in aliquam. Eu tincidunt tortor aliquam nulla facilisi cras fermentum odio. Et sollicitudin ac orci phasellus. Rutrum tellus pellentesque eu tincidunt tortor aliquam nulla facilisi. Pharetra convallis posuere morbi leo urna molestie. Malesuada nunc vel risus commodo viverra. Eleifend donec pretium vulputate sapien. Mattis molestie a iaculis at erat pellentesque. Tortor pretium viverra suspendisse potenti nullam ac. Vehicula ipsum a arcu cursus. Ac tortor vitae purus faucibus ornare suspendisse sed nisi lacus. Interdum consectetur libero id faucibus nisl tincidunt eget nullam non.'
-                }
-             ,
-            
-                {
-            nombre:"Fresh Stripe Sweater",
-            imagenNo1:'https://imagizer.imageshack.com/img924/925/TDrFPi.png',
-            imagenNo2:'https://imagizer.imageshack.com/img923/7853/Qri38y.png',
-            imagenNo3:'https://imagizer.imageshack.com/img922/6748/Jmu58N.png',
-            tallaXS: 55000 ,
-            tallaS: 80000 ,
-            tallaM: 82000 ,
-            tallaL: 85000 ,
-            tallaXL: 90000 ,
-            tallaXXL: 93000 ,
-            inventario: 2,
-            descripcion: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Nibh venenatis cras sed felis eget. At in tellus integer feugiat scelerisque varius morbi. Morbi tempus iaculis urna id volutpat. Eget nunc scelerisque viverra mauris in aliquam. Eu tincidunt tortor aliquam nulla facilisi cras fermentum odio. Et sollicitudin ac orci phasellus. Rutrum tellus pellentesque eu tincidunt tortor aliquam nulla facilisi. Pharetra convallis posuere morbi leo urna molestie. Malesuada nunc vel risus commodo viverra. Eleifend donec pretium vulputate sapien. Mattis molestie a iaculis at erat pellentesque. Tortor pretium viverra suspendisse potenti nullam ac. Vehicula ipsum a arcu cursus. Ac tortor vitae purus faucibus ornare suspendisse sed nisi lacus. Interdum consectetur libero id faucibus nisl tincidunt eget nullam non.'
-            }
-         ,
-            
-        {   nombre:"Fresh Black Tie-Dye Hoodie",
-            imagenNo1:'https://imagizer.imageshack.com/img923/539/Fj2CQP.png',
-            imagenNo2:'https://imagizer.imageshack.com/img922/3017/IrsV0Q.png',
-            imagenNo3:'https://imagizer.imageshack.com/img922/4887/7Y9qeW.png',
-            tallaXS: 55000 ,
-            tallaS: 60000 ,
-            tallaM: 70000 ,
-            tallaL: 75000 ,
-            tallaXL: 80000 ,
-            tallaXXL: 90000 ,
-            inventario: 6,
-            descripcion: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Nibh venenatis cras sed felis eget. At in tellus integer feugiat scelerisque varius morbi. Morbi tempus iaculis urna id volutpat. Eget nunc scelerisque viverra mauris in aliquam. Eu tincidunt tortor aliquam nulla facilisi cras fermentum odio. Et sollicitudin ac orci phasellus. Rutrum tellus pellentesque eu tincidunt tortor aliquam nulla facilisi. Pharetra convallis posuere morbi leo urna molestie. Malesuada nunc vel risus commodo viverra. Eleifend donec pretium vulputate sapien. Mattis molestie a iaculis at erat pellentesque. Tortor pretium viverra suspendisse potenti nullam ac. Vehicula ipsum a arcu cursus. Ac tortor vitae purus faucibus ornare suspendisse sed nisi lacus. Interdum consectetur libero id faucibus nisl tincidunt eget nullam non.' 
-        }
-    ,
-     
-        {   nombre:"Pose For It Tee",
-            imagenNo1:'https://imagizer.imageshack.com/img923/7109/1Bc1h0.png',
-            imagenNo2:'https://imagizer.imageshack.com/img924/8972/0zBQMN.png',
-            imagenNo3:'https://imagizer.imageshack.com/img923/3486/ofR9Ai.png',
-            tallaXS: 55000 ,
-            tallaS: 57000 ,
-            tallaM: 60000 ,
-            tallaL: 62000 ,
-            tallaXL: 70000 ,
-            tallaXXL: 75000 ,
-            inventario: 20,
-            descripcion: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Nibh venenatis cras sed felis eget. At in tellus integer feugiat scelerisque varius morbi. Morbi tempus iaculis urna id volutpat. Eget nunc scelerisque viverra mauris in aliquam. Eu tincidunt tortor aliquam nulla facilisi cras fermentum odio. Et sollicitudin ac orci phasellus. Rutrum tellus pellentesque eu tincidunt tortor aliquam nulla facilisi. Pharetra convallis posuere morbi leo urna molestie. Malesuada nunc vel risus commodo viverra. Eleifend donec pretium vulputate sapien. Mattis molestie a iaculis at erat pellentesque. Tortor pretium viverra suspendisse potenti nullam ac. Vehicula ipsum a arcu cursus. Ac tortor vitae purus faucibus ornare suspendisse sed nisi lacus. Interdum consectetur libero id faucibus nisl tincidunt eget nullam non.' 
-        }
-    ]
-
-
-function compraRealizada(productoNumero, cantidad){
-    productoNumero.inventario = productoNumero.inventario - cantidad
+import bodega from './inventario.js'
+document.querySelector('#min1').addEventListener('click',agrandarMiniatura)
+document.querySelector('#min2').addEventListener('click',agrandarMiniatura)
+document.querySelector('#min3').addEventListener('click',agrandarMiniatura)
+document.querySelector('#precioPrenda').innerHTML = '$' + bodega[0].tallaM
+document.querySelector('#precioInter').innerHTML = '$ '+bodega[0].tallaM
+document.querySelector('#precioInter').innerHTML = '$ '+bodega[0].tallaM
+document.querySelector('#precioInter').innerHTML = '$ '+bodega[1].tallaM
+document.querySelector('#precio2').innerHTML = '$ '+bodega[2].tallaM
+document.querySelector('#precio3').innerHTML = '$ '+bodega[3].tallaM
+function agrandarMiniatura(){
+    document.querySelector('#bigPhoto').src = this.getAttribute('src')
 }
-export default bodega
+let seleccion = 0
+document.querySelectorAll('#moreClothes')[0].addEventListener('click',cambiarCatalogo)
+document.querySelectorAll('#moreClothes')[1].addEventListener('click',cambiarCatalogo)
+document.querySelectorAll('#moreClothes')[2].addEventListener('click',cambiarCatalogo)
+
+function cambiarCatalogo(){
+
+     seleccion = this.getAttribute('index')
+    
+    if (seleccion==1){
+        if(document.querySelectorAll('#moreClothes')[0].src== bodega[1].imagenNo2 ){
+            document.querySelectorAll('#moreClothes')[0].src = bodega[0].imagenNo1
+            document.querySelector('#textoInter').innerHTML = bodega[0].nombre
+            document.querySelector('#precioInter').innerHTML = '$ '+bodega[0].tallaM
+            console.log(textoInter.innerHTML)
+            // console.log(bodega[0].)
+        }else{
+            document.querySelectorAll('#moreClothes')[0].src =bodega[1].imagenNo2
+            document.querySelector('#textoInter').innerHTML = bodega[1].nombre
+            document.querySelector('#precioInter').innerHTML = '$ '+bodega[1].tallaM
+            seleccion = 0
+            }
+    }    
+    document.querySelector('#min1').src = bodega[seleccion].imagenNo1
+    document.querySelector('#min2').src = bodega[seleccion].imagenNo2
+    document.querySelector('#min3').src = bodega[seleccion].imagenNo3
+    document.querySelector('#bigPhoto').src = bodega[seleccion].imagenNo1
+    document.querySelector('#nombrePrenda').innerHTML = bodega[seleccion].nombre
+    document.querySelector('#precioPrenda').innerHTML = '$' + bodega[seleccion].tallaM
+    window.scrollTo(0, 0)
+}
+document.querySelector('#s-xs').addEventListener('click',cambioPrecio)
+document.querySelector('#s-s').addEventListener('click',cambioPrecio)
+document.querySelector('#s-m').addEventListener('click',cambioPrecio)
+document.querySelector('#s-l').addEventListener('click',cambioPrecio)
+document.querySelector('#s-xl').addEventListener('click',cambioPrecio)
+document.querySelector('#s-xxl').addEventListener('click',cambioPrecio)
+
+function cambioPrecio (){
+    
+    let talla = this.getAttribute('id')
+    console.log(seleccion)
+    
+    function preciosTalla(talla) {
+        const tamaños = {
+            's-xs': bodega[seleccion].tallaXS,
+            's-s': bodega[seleccion].tallaS,
+            's-m': bodega[seleccion].tallaM,
+            's-l': bodega[seleccion].tallaL,
+            's-xl': bodega[seleccion].tallaXL,
+            's-xxl': bodega[seleccion].tallaXXL,
+        } 
+        return tamaños[talla]
+    }
+    
+    document.querySelector('#precioPrenda').innerHTML = '$' + preciosTalla(talla)
+    
+   
+    
+}                        
